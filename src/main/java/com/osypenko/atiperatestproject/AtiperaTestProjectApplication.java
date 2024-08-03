247a200d151d81fbfe38dff94743568f2d07fb21
oleksandrosypenko <Oleksandrosypenko1989@gmail.com>
2024-08-03 16:03:52 +0200

    Create test project

---
+++ src/main/java/com/osypenko/atiperatestproject/AtiperaTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.osypenko.atiperatestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtiperaTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osypenko/atiperatestproject/AtiperaTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osypenko.atiperatestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
